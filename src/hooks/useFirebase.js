2c5fa7e75012b2ff41637bda33a6a664e2cdd27e
jose luis <jdesignermayor@gmail.com>
2022-03-31 19:30:53 -0500

    feat: :sparkles: add form component and modify firebase hook

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,38 +1,32 @@
 
 import { db, auth } from '../firebaseConfig';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, setDoc, doc } from 'firebase/firestore';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 export function useFirebase(){
 
     async function setAuthToken(email, password){
-        try {
-            const credentials = await createUserWithEmailAndPassword(auth, email, password);
-            const authData = credentials.user;
-            return authData;
-        } catch (e) {
-            return new Error('Some error:', e);
-        }
+        const credentials = await createUserWithEmailAndPassword(auth, email, password);
+        const authData = credentials?.user;
+        return authData;
     }
 
-    async function saveCustomer(){
-
+    async function saveCustomer(props){
+        const result = await setDoc(doc(db, 'customer'), props);
+        return result;
     }
 
     async function getCostumers(){
         const customerCol = collection(db, 'customer');
         const customerSnapshot = await getDocs(customerCol);
         const customerList = customerSnapshot.docs.map(doc => doc.data());
-        
         return customerList;
     }
 
     async function setCustomer(props){
-        const { email, password } = props;
-        const authData = await setAuthToken(email, password);
-       
-
-        console.log('costumerData:', props);
+        const { companyEmail, password } = props;
+        // const authData = await setAuthToken(companyEmail, password);
+        await setDoc(doc(db, 'customer'), {props});
     }
 
     return {
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,47 +1,122 @@
+import { useState } from 'react';
 import { useFormik } from 'formik';
 
 import { Container, Grid, Input, FormControl,
     FormLabel,
-    Button
+    Button,
 } from '@chakra-ui/react';
 
 import { useFirebase } from '../hooks/useFirebase';
 
+import * as Yup from 'yup';
+
 const initialProps = {
-    email: '',
+    companyName: '',
+    companyEmail: '',
+    contactName: '',
+    companyPhone: '',
+    companyEmployeeTotal: '',
+    nit: '',
+    address: '',
+    city: '',
+    province: '',
+    economyActivity: '',
     password: '',
     uid: ''
 };
 
-export const Register = () => {
+const SignupSchema = Yup.object().shape({
+    companyName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+    companyEmail: Yup.string().email('Invalid email').required('Required'),
+    contactName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+    companyPhone: Yup.number()
+    .min(2, 'Too Short!')
+    .required('Required'),
+    companyEmployeeTotal: Yup.number()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+    companyEmployeeTotal: Yup.number()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+    password: Yup.string()
+    .min(8, 'Too Short!')
+    .required('Required')
+    .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.')
+  });
 
+export const Register = () => {
+    const [ isLoading, setIsloading ] = useState(false);
     const { setCustomer } = useFirebase();
 
-    const formik = useFormik({
+    const { handleSubmit, handleChange, values, errors, touched } = useFormik({
         initialValues: initialProps,
+        validationSchema: SignupSchema,
         onSubmit: (values) => {
+            setIsloading(true);
             setCustomer(values).then(() => {
-              
+                setIsloading(false);
             }).catch(() => {
                 alert("erorr");
+                setIsloading(false);
             });
-            
         },
     });
 
   return (
     <Container>
-            <form onSubmit={formik.handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <Grid gap={3}>
                     <FormControl>
-                        <FormLabel htmlFor="email">Correo empresa</FormLabel>
-                        <Input id="email" type="email"  name="email" value={formik.values.email} onChange={formik.handleChange} />
+                        <FormLabel htmlFor="companyName">Nombre empresa</FormLabel>
+                        <Input id="companyName" type="text"  name="companyName" value={values.companyName} onChange={handleChange} />
+                        {errors.companyName && touched.companyName ? (
+                            <div>{errors.companyName}</div>
+                        ) : null}
                     </FormControl>
                     <FormControl>
-                        <FormLabel htmlFor="password">clave empresa</FormLabel>
-                        <Input id="password" type="text" value={formik.values.password} onChange={formik.handleChange} />
+                        <FormLabel htmlFor="contactName">Nombre de contacto</FormLabel>
+                        <Input id="contactName" type="text" name="contactName" value={values.contactName} onChange={handleChange} />
+                        {errors.contactName && touched.contactName ? (
+                            <div>{errors.contactName}</div>
+                        ) : null}
                     </FormControl>
-                    <Button type="submit">Registrarse</Button>
+                    <FormControl>
+                        <FormLabel htmlFor="companyEmail">Correo de contacto</FormLabel>
+                        <Input id="companyEmail" type="email"  name="companyEmail" value={values.companyEmail} onChange={handleChange} />
+                        {errors.companyEmail && touched.companyEmail ? (
+                            <div>{errors.companyEmail}</div>
+                        ) : null}
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel htmlFor="companyPhone">Teléfono empresa</FormLabel>
+                        <Input id="companyPhone" type="phone" name="companyPhone" value={values.companyPhone} onChange={handleChange} />
+                        {errors.companyPhone && touched.companyPhone ? (
+                            <div>{errors.companyPhone}</div>
+                        ) : null}
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel htmlFor="companyEmployeeTotal">Número de empleados</FormLabel>
+                        <Input id="companyEmployeeTotal" type="number"  name="companyEmployeeTotal" value={values.companyEmployeeTotal} onChange={handleChange} />
+                        {errors.companyEmployeeTotal && touched.companyEmployeeTotal ? (
+                            <div>{errors.companyEmployeeTotal}</div>
+                        ) : null}
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel htmlFor="password">Contraseña</FormLabel>
+                       <Input id="password" type="text" value={values.password} onChange={handleChange} />
+                        {errors.password && touched.password ? (
+                            <div>{errors.password}</div>
+                        ) : null}
+                    </FormControl>
+                    <Button isLoading={isLoading} type="submit">Registrarse</Button>
                 </Grid>
             </form>
     </Container>
