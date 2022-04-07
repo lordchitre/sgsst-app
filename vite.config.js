f64446487608826f9f31744395d8390517a2f8ab
jose luis <jdesignermayor@gmail.com>
2022-04-07 13:32:53 -0500

    updated vitejs for build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
 
 export default defineConfig({
   esbuild: {
     jsxFactory: '_jsx',
     jsxFragment: '_jsxFragment',
+    jsxInject: `import { createElement as _jsx, Fragment as _jsxFragment } from 'react'`,
   },
-  plugins: [react()],
+  plugins: [reactRefresh()],
   server: {
     host: true,
   },
