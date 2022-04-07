ddae4687eb5dc8ce23c714767dc895a24465859a
jose luis <jdesignermayor@gmail.com>
2022-04-07 16:32:00 -0500

    feat: :sparkles: add redux in our app to control the state management

---
+++ src/redux/authSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const autSlice = createSlice({
+  name: "auth",
+  initialState: {
+    isAuthenticated: false,
+    authData: null,
+  },
+  reducers: {
+    onAuthenticate : (state, action) => {
+        state = {
+            isAuthenticated: true,
+            authData: action.payload
+        };
+    }
+  }
+}) 
+
+export const { onAuthenticate } = autSlice.actions;
+export const getAuthentication = (state) => state.auth;
+
+export default autSlice.reducer;
