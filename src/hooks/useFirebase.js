b36d5b495809d751108805c6f4d37817c7d5be50
jose luis <jdesignermayor@gmail.com>
2022-04-07 17:26:04 -0500

    feat: :fire: update code for create and validate the authentication correctly

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,40 +1,52 @@
 
-import { db, auth } from '../firebaseConfig';
+import { db, auth, tables } from '../firebaseConfig';
+
 import { collection, getDocs, addDoc } from 'firebase/firestore';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, deleteUser, getAuth } from 'firebase/auth';
 
-export function useFirebase(){
+export function useFirebase() {
 
-    async function setAuthToken(email, password){
-        const credentials = await createUserWithEmailAndPassword(auth, email, password);
-        const authData = credentials?.user;
-        return authData;
+    async function createAuthentication(email, password) {
+        return await createUserWithEmailAndPassword(auth, email, password);
     }
 
-    async function saveCustomer(props){
+    async function saveCustomer(props, user = null) {
+        const uid = user?.uid;
+
         const prospectData = {
             ...props,
+            uid,
         };
-        
-        const customerCol = collection(db, 'customer');
 
+        const customerCol = collection(db, tables.customer);
         return await addDoc(customerCol, prospectData);
     }
 
-    async function getCostumers(){
-        const customerCol = collection(db, 'customer');
+    async function getCostumers() {
+        const customerCol = collection(db, tables.customer);
         const customerSnapshot = await getDocs(customerCol);
         const customerList = customerSnapshot.docs.map(doc => doc.data());
         return customerList;
     }
 
-    async function setCustomer(props){
-        saveCustomer(props);
+    async function setCustomer(props) {
+        const { companyEmail, password } = props;
+
+        await createAuthentication(companyEmail, password).then(({ user }) => {
+            saveCustomer(props, user).then((response) => {
+                return Promise.resolve(user);
+            }).catch((err) => {
+                console.error(err);
+                return Promise.reject(err);
+            });
+        }).catch((err) => {
+            return Promise.reject(err);
+        });
     }
 
     return {
         getCostumers,
         setCustomer
-    }   
+    }
 }
 
